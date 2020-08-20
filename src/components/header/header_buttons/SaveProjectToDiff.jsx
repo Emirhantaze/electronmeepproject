baef2c900aacdc88010077e4d81a4ed41b33896a
emtiyl <emtiyltezkry@gmail.com>
2020-08-20 12:46:36 +0300

    Create SaveProjectToDiff.jsx

---
+++ src/components/header/header_buttons/SaveProjectToDiff.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useContext } from 'react';
+import { SimContext } from '../../../context/Sim';
+const {remote} = window.require('electron')
+const { dialog } = remote.require('electron')
+const fs = remote.require('fs')
+const win = remote.getCurrentWindow();
+export const SaveProjectToDiff = () => {
+    const {sim} = useContext(SimContext)
+    return (
+        <button onClick={
+            async ()=>{
+                const  options = {
+                        title: "Select folder for saved simulation",
+                        properties:["openDirectory"]
+                    }   
+                const path = await dialog.showOpenDialog(win,options)
+                fs.writeFileSync(path.filePaths[0]+`/project.json`, JSON.stringify(sim), 'utf8', ()=>{});
+            }
+        }>save simulation file</button>
+    )
+}
