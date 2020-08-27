fdece4c24c2bb210adf52b30aba1751146683256
Emirhan Taze <emtiyltezkry@gmail.com>
2020-08-27 11:22:29 +0300

    removing predefined  img file reading from view port

--- src/components/main/Viewport.jsx
+++ src/components/main/Viewport.jsx
@@ -1,10 +1,8 @@
 import React, { } from 'react'
-const fs = window.require(`fs`)
-var _img = fs.readFileSync("/home/emirhantaze/test.svg").toString('base64');
 const Viewport = () => {
     return (
         <div style={{ maxWidth: "400px", float: "left" }}
-        ><img src={`data:image/svg+xml;base64,${_img}`} alt=""/></div>
+        ><img src={`data:image/svg+xml;base64,${"_"}`} alt=""/></div>
     );
 };
 export default Viewport;
