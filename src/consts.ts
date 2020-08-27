c396283f4e024df75047667a6abf0b85852ae442
Emirhan Taze <emtiyltezkry@gmail.com>
2020-08-27 22:56:25 +0300

    update

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+import { EMPTYSIMULATION } from "./meepInterfaces";
+
+export const EMPTYPROJECT = [
+	{
+		name: "undefined",
+		type: "simulaton",
+		values: {},
+	},
+] as Array<EMPTYSIMULATION>;
